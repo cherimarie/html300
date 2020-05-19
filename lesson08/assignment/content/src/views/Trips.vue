b6a7ab324924cea13e7924c469f203a8a1f25d64
CheriMarie <cherimarie@gmail.com>
2020-05-19 12:39:37 -0700

    Search functional

--- lesson08/assignment/content/src/views/Trips.vue
+++ lesson08/assignment/content/src/views/Trips.vue
@@ -2,7 +2,9 @@
     <div class="Trips">
         <b-row>
             <b-col lg = '2' sm="12">
-                <search :decks='flist'></search>
+                <!-- <search :decks='flist'></search> -->
+                <input type="text" v-model="search" placeholder="Search location..."/>
+                <label>Search title:</label>
             </b-col>
             <b-col lg = '10' sm="12">
                 <div class='card-multiple'>
@@ -22,24 +24,24 @@
 import { decks } from '../util.js'
 
 import Card from '@/components/Card.vue'
-import Searchbar from '@/components/Searchbar.vue'
+// import Searchbar from '@/components/Searchbar.vue'
 
 export default {
     name: 'Trips',
     components: {
-        'card-single': Card,
-        'search': Searchbar
+        'card-single': Card
     },
     data() {
         return {
-
+          search: '',
+          decks: decks
         }
     },
     computed: {
       flist: function(){
-        return decks.filter(function (litem) {
-            return litem.area != null;
-        })
+        return this.decks.filter(function (litem) {
+            return (litem.area != null) && (litem.location.toLowerCase().includes(this.search.toLowerCase()))
+        }, this)
       }
     }
 };
